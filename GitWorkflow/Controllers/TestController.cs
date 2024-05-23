f51a5bd55e3b8e2003818d9935ec8eb74b66554b
kadirkurhan <kadirkurhan42@hotmail.com>
2024-05-23 23:22:11 +0300

    first commit

---
+++ GitWorkflow/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitWorkflow.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    public TestController()
+    {
+    }
+
+    [HttpGet]
+    public string Get(int request)
+    {
+        if(request % 2 == 0)
+        {
+            return "odd";
+        }
+
+        return "even";
+    }
+}
+
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace Test;
+using GitWorkflow.Controllers;
+
+public class UnitTest1
+{
+    [Fact]
+    public void OK_WhenReturnOdd_GetEndpoint()
+    {
+        // Arrange
+        var controller = new TestController();
+
+        // Aspect
+        var response = controller.Get(1);
+
+        // Assert
+        Assert.Equal("even", response);
+    }
+}
